3bbaaca1fb94656403222c33fe99f8d69916e783
Guillaume KESTEMAN <guillaume@les-tilleuls.coop>
2022-06-29 12:13:11 +0200

    Add constraints when media object creation

--- src/Controller/CreateMediaObjectActionController.php
+++ src/Controller/CreateMediaObjectActionController.php
@@ -7,15 +7,38 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\Validator\Constraints\Image;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\ConstraintViolation;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 #[AsController]
 final class CreateMediaObjectActionController extends AbstractController
 {
-    public function __invoke(Request $request): MediaObject
+    public function __invoke(Request $request, ValidatorInterface $validator): MediaObject
     {
         $uploadedFile = $request->files->get('file');
-        if (!$uploadedFile) {
-            throw new BadRequestHttpException('"file" is required');
+
+        $violations = $validator->validate($uploadedFile, [
+                new NotBlank(),
+                new Image([
+                    'maxSize' => "5M",
+                    'minWidth' => 100,
+                    'maxWidth' => 2000,
+                    'minHeight' => 100,
+                    'maxHeight' => 2000,
+                    'mimeTypes' => [
+                        "image/jpeg",
+                        "image/jpg",
+                        "image/png",
+                    ],
+                ])]
+        );
+
+        if ($violations->count() > 0) {
+            /** @var ConstraintViolation $violation */
+            $violation = $violations[0];
+            throw new BadRequestHttpException($violation->getMessage());
         }
 
         $mediaObject = new MediaObject();
