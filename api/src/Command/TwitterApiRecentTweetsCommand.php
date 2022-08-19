0c259a3284717885dad8f83010224001c2747c15
Guillaume KESTEMAN <kesteman.guillaume@gmail.com>
2022-08-19 11:27:58 +0200

    Move twitter command constants to the top of the class

--- api/src/Command/TwitterApiRecentTweetsCommand.php
+++ api/src/Command/TwitterApiRecentTweetsCommand.php
@@ -43,6 +43,13 @@ use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
 )]
 final class TwitterApiRecentTweetsCommand extends Command
 {
+    private const DEFAULTS_TWEETS_REPLIES = [
+        ['id' => 'on_new_game', 'reply' => 'Hey %nom% (%@joueur%), merci de participer à notre jeu ! ' . \PHP_EOL . 'Pour avoir plus d\'informations sur le jeu voici notre site web : %site_web%'],
+        ['id' => 'game_already_generated_less_than_a_day_ago', 'reply' => 'Merci %nom% (%@joueur%) de parler de nous.' . \PHP_EOL . 'Malheureusement tu as déjà joué il y a moins de 24h, tu pourras rejouer une fois que cela fera plus d\'une journée ! ' . \PHP_EOL . 'Pour plus d\'informations tu peux consulter notre site web : %site_web%'],
+        ['id' => 'need_to_follow_us', 'reply' => 'Merci %nom% (%@joueur%) de parler de nous. ' . \PHP_EOL . 'Malheureusement tu n\'es pas encore éligible pour pouvoir participer au jeu. Pour l\'être tu dois suivre les comptes nécessaires. ' . \PHP_EOL . 'Pour plus d\'informations tu peux consulter notre site web : %site_web%'],
+        ['id' => 'no_more_available_lots', 'reply' => 'Nous n\'avons malheureusement plus aucun lot de disponible... ' . \PHP_EOL . 'Retente ta chance un autre jour !'],
+    ];
+
     public function __construct(
         private readonly TwitterApi                       $twitterApi,
         private readonly PlayerRepository                 $playerRepository,
@@ -67,13 +74,6 @@ final class TwitterApiRecentTweetsCommand extends Command
             ->addOption('update-db', null, InputOption::VALUE_NONE, 'Update database')
             ->addOption('reply', null, InputOption::VALUE_NONE, 'Reply with game URL');
     }
-
-    private const DEFAULTS_TWEETS_REPLIES = [
-        ['id' => 'on_new_game', 'reply' => 'Hey %nom% (%@joueur%), merci de participer à notre jeu ! ' . \PHP_EOL . 'Pour avoir plus d\'informations sur le jeu voici notre site web : %site_web%'],
-        ['id' => 'game_already_generated_less_than_a_day_ago', 'reply' => 'Merci %nom% (%@joueur%) de parler de nous.' . \PHP_EOL . 'Malheureusement tu as déjà joué il y a moins de 24h, tu pourras rejouer une fois que cela fera plus d\'une journée ! ' . \PHP_EOL . 'Pour plus d\'informations tu peux consulter notre site web : %site_web%'],
-        ['id' => 'need_to_follow_us', 'reply' => 'Merci %nom% (%@joueur%) de parler de nous. ' . \PHP_EOL . 'Malheureusement tu n\'es pas encore éligible pour pouvoir participer au jeu. Pour l\'être tu dois suivre les comptes nécessaires. ' . \PHP_EOL . 'Pour plus d\'informations tu peux consulter notre site web : %site_web%'],
-        ['id' => 'no_more_available_lots', 'reply' => 'Nous n\'avons malheureusement plus aucun lot de disponible... ' . \PHP_EOL . 'Retente ta chance un autre jour !'],
-    ];
 
     private function selectDefaultTweetReplieById(string $id): ?array
     {
