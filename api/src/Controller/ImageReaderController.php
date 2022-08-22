16511a280c743b05531ab4460dd7f0d412f12705
Guillaume KESTEMAN <kesteman.guillaume@gmail.com>
2022-08-22 15:50:00 +0200

    Update imageReaderController : use of flysystem & Exception management with logger

--- api/src/Controller/ImageReaderController.php
+++ api/src/Controller/ImageReaderController.php
@@ -5,20 +5,39 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Entity\MediaObject;
+use League\Flysystem\FilesystemException;
+use League\Flysystem\FilesystemOperator;
+use League\Flysystem\UnableToReadFile;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 final class ImageReaderController extends AbstractController
 {
-    #[Route('/image/{filePath}', name: 'app_image_reader')]
-    public function indexReader(MediaObject $image): BinaryFileResponse
+    public function __construct(private readonly FilesystemOperator $defaultMedia, private readonly LoggerInterface $logger)
     {
-        $root = explode('/', __DIR__);
-        $root = \array_slice($root, 0, -2);
-        $root = implode('/', $root);
-        $filename = '/public/media/'.$image->getFilePath();
+    }
 
-        return new BinaryFileResponse($root.$filename);
+    /**
+     * @throws FilesystemException
+     */
+    #[Route('/image/{filePath}', name: 'app_image_reader')]
+    public function indexReader(MediaObject $image): Response
+    {
+        $imageContent = null;
+
+        try {
+            $imageContent = $this->defaultMedia->read($image->getFilePath());
+        } catch (FilesystemException | UnableToReadFile $e) {
+            $this->logger->error('Error on /image/'.$image->getFilePath().' : "'.$e->getMessage().'"', (array) $e);
+            throw $e;
+        }
+
+        $headers = [
+            'Content-Type' => 'image/*',
+        ];
+
+        return new Response($imageContent, 200, $headers);
     }
 }
