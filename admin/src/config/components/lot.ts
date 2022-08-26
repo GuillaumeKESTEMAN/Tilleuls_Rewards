5514e0ed960b4b5a56a97b4874e659840ad029a4
Guillaume KESTEMAN <kesteman.guillaume@gmail.com>
2022-08-26 09:35:38 +0200

    Update admin lot message placeholder

--- admin/src/config/components/lot.ts
+++ admin/src/config/components/lot.ts
@@ -1,2 +1,2 @@
-export const MESSAGE_PLACE_HOLDER = 'Hey %@joueur%, you won with a score of : %score% !\nWith that score you win : ';
+export const MESSAGE_PLACE_HOLDER = 'Hey %@joueur%, tu as gagné ta partie avec un score de %score% points !\nMerci d\'avoir participé à notre jeu, pour te féliciter nos t\'offrons  un lot !';
 export const MESSAGE_HELPER = '%@joueur% = mentionne le joueur, %nom% = écrit le nom du joueur, %score% = écrit son score';
